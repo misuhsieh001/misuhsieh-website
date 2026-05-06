66585f08dc07dbcb933e90c159d8a7158d6e1b66
Vincent Hsieh <vincenthsieh@Vincents-MacBook-Air.local>
2026-05-07 02:08:15 +0800

    Add English

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind"; // 1. 確保這行一定要有
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // 2. 確保 integrations 這個區塊要有出現
   integrations: [tailwind()],
+  // 新增下面這個 i18n 區塊
+  i18n: {
+    defaultLocale: 'en',       // 預設語言是繁體中文
+    locales: ['zh-tw', 'en'],     // 支援中文與英文
+    routing: {
+      prefixDefaultLocale: false  // 中文網址不需要加 /zh-tw/，只有英文才加 /en/
+    }
+  }
 });
